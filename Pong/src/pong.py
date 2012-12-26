c4396a6502f21b90675fae0f38c875b5326c415f
Isaac Torres <ismatorr@gmail.com>
2012-12-26 03:07:39 -0500

    Commit inicial

---
+++ Pong/src/pong.py
@@ -0,0 +1,8 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="Isaac"
+__date__ ="$26-dic-2012 2:58:53$"
+
+if __name__ == "__main__":
+    print "Hello World"
