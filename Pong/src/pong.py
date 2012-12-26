ec3d97266ee68334226ae383243c7c7ee8f813ba
Isaac Torres <ismatorr@gmail.com>
2012-12-26 03:13:50 -0500

    funcion para cargar imagen usando pygame

--- Pong/src/pong.py
+++ Pong/src/pong.py
@@ -1,8 +1,21 @@
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
 
-__author__="Isaac"
-__date__ ="$26-dic-2012 2:58:53$"
+import pygame
+from pygame.locals import *
 
-if __name__ == "__main__":
-    print "Hello World"
+WIDTH = 640
+HEIGHT = 480
+
+def load_image(filename, transparent=False):
+    try: image = pygame.image.load(filename)
+    except pygame.error, message:
+        raise SystemExit, message
+    image = image.convert()
+    if transparent:
+        color = image.get_at((0,0))
+        image.set_colorKey(color, RLEACCEL)
+
+return image
+
+
