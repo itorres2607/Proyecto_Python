2b4250731ffdaed8d85a873804b4ff91bcd40a34
Isaac Torres <ismatorr@gmail.com>
2013-01-02 01:07:30 -0500

    implementacion de la clase ball y mostrarla en pantalla

--- Pong/src/pong.py
+++ Pong/src/pong.py
@@ -7,6 +7,43 @@ from pygame.locals import *
 WIDTH = 640
 HEIGHT = 480
 
+#classes
+#-----------------------------------------------------------------------
+
+class ball(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = load_image("images/ball.png", True)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.centery = HEIGHT / 2
+        self.speed = [0.5,-0.5]
+
+    def actualizar(self , time):
+        self.rect.centerx += self.speed[0] * time
+        self.rect.centery += self.speed[1] * time
+        if self.rect.left <= 0 or self.rect.right >= WIDTH:
+            self.speed[0] = - self.speed[0]
+            self.rect.centerx += self.speed[0] * time
+        if self.rect.top <= 0 or self.rect.bottom >= HEIGHT:
+            self.speed[1] = - self.speed[1]
+            self.rect.centery += self.speed[1] * time
+
+
+
+
+
+
+
+#------------------------------------------------------------------------
+
+
+
+
+
+#functions
+#-----------------------------------------------------------------------------------
+
 def load_image(filename, transparent=False):
     try: image = pygame.image.load(filename)
     except pygame.error, message:
@@ -14,20 +51,39 @@ def load_image(filename, transparent=False):
     image = image.convert()
     if transparent:
         color = image.get_at((0,0))
-        image.set_colorKey(color, RLEACCEL)
+        image.set_colorkey(color, RLEACCEL)
 
-return image
+    return image
+
 
 def main():
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Proyecto")
+
+    background_image = load_image('images/fondo_pong.png')
+    bola = ball()
+
+    clock = pygame.time.Clock()
+
+
+
     while True:
+        time = clock.tick(60)
         for eventos in pygame.event.get():
             if eventos.type == QUIT:
                 sys.exit(0)
-return 0
+
+        bola.actualizar(time)
+        screen.blit(background_image, (0,0))
+        screen.blit(bola.image, bola.rect)
+        pygame.display.flip()
+
+    return 0
+
+#---------------------------------------------------------------------------------------------------
 
 
 if __name__ == "__main__":
     pygame.init()
     main()
+
